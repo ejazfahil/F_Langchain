13311b06b66879c0b4442ca337205eb2949083af
Fahil Ejaz <ejazfahil@gmail.com>
2025-05-02 17:45:00 +0200

    feat: conversation memory wrapper

---
+++ src/memory_chain.py
@@ -0,0 +1,8 @@
+"""Conversation with memory. 2025-05-02"""
+def build_chat_chain(api_key:str, use_summary:bool=True):
+    from langchain_openai import ChatOpenAI
+    from langchain.chains import ConversationChain
+    from langchain.memory import ConversationBufferMemory, ConversationSummaryMemory
+    llm=ChatOpenAI(model="gpt-4o-mini",api_key=api_key,temperature=0.7)
+    mem=(ConversationSummaryMemory(llm=llm) if use_summary else ConversationBufferMemory())
+    return ConversationChain(llm=llm,memory=mem,verbose=False)
